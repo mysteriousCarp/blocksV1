6898dc641b993a80cb77c0f8d7318b1c0c016bef
mysteriousCarp <mysteriouskarp@gmail.com>
2025-01-08 02:34:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,227 +1,3 @@
-// const testProgram = [
-//     [
-//         {"type": "motor", "id": "AB", "power": 100, },
-//         {"type": "wait", "cond": "time", "time": 1000},
-//         {"type": "motor", "id": "AB", "power": 0}
-//     ]
-// ];
-// paramstest = {len: 2,
-//   first: 
-//     {type: 'choose',
-//     data: 'ABC'},
-//   second: 
-//     {type: 'int',
-//     data: 'get power'}}
-// var currentid = 0
-// var blocks = []
-// function getRandomInt() {
-//   return Math.floor(Math.random() * 256);
-// }
-// function removeElementFromArray(array, element) {
-//   const index = array.indexOf(element);
-//   if (index > -1) {
-//       array.splice(index, 1);
-//       console.log(`Элемент '${element}' был удалён из массива.`);
-//       return true
-//   } else {
-//     return false  
-//     console.log(`Элемент '${element}' не найден в массиве.`);
-//   }
-//   return array;
-// }
-// class block
-// {
-//   constructor(tip, params, parent)
-//   {
-//     this.tip = toString(tip);
-//     this.id = currentid + 'cl';
-//     this.params = params;
-//     this.parent = parent;
-//     this.createBlock(tip, params, parent);
-//     blocks.push(this);
-//     this.kids = [];
-//     this.parent = null;
-
-//     this.size = 100
-//     this.element.style.height = this.size + 'px';
-//     this.element.style.backgroundColor = 'rgb(' + getRandomInt() + ',' + getRandomInt() + ',' + getRandomInt() + ')'
-//   };
-
-//   makeDraggable(element) {
-//     this.isDragging = false;
-//     let offsetX, offsetY;
-
-//     element.addEventListener('mousedown', (e) => {
-//       this.isDragging = true;
-//       offsetX = e.clientX - element.offsetLeft;
-//       offsetY = e.clientY - element.offsetTop;
-//     });
-
-//     document.addEventListener('mouseup', (e)=> {
-
-//       if (this.isDragging){
-//         this.isDragging = false;
-//         for (var i = 0; i < currentid; i++)
-//         {
-//           const blockz = blocks[i];
-//           var d = this.id;
-//           console.log(blockz)
-//           console.log(e)
-//           let intervalId = setInterval((e) => {
-//             if ( (typeof d !== 'undefined' && typeof blockz.id !== 'undefined')) {
-//                 console.log('Переменная d существует:', d);
-//                 clearInterval(intervalId); // Останавливаем проверку
-//                 if (this.parent != null){
-//                 if (removeElementFromArray(this.parent.kids, this)){this.parent = null}
-
-//                 }
-//                 if (blockz.id != d)
-//                   {
-
-//                     if (blockz.contact(e, this.size))
-//                     {
-//                       var cordss = blockz.getAbsoluteCoordinates(document.body.id, blockz.id);
-//                       console.log(cordss);
-//                       this.element.style.left = cordss.x + this.size + 'px'
-//                       this.element.style.top = cordss.y + 'px'
-//                       console.log(blocks)
-//                       blockz.kids.unshift(this)
-//                       this.parent = blockz;
-//                     }
-//                   }
-//             }
-//         }, 100);
-
-//       }
-//     }
-//     });
-
-//     document.addEventListener('mousemove', (e) => {
-//       globalThis.pos = e
-//       if (!this.isDragging) return;
-//       this.element.style.left = (e.clientX - offsetX) + 'px';
-//       this.element.style.top = (e.clientY - offsetY) + 'px';
-//     });
-//   };
-
-//   createBlock(tip, params, parent)
-//   {
-//     const newBlock = document.createElement('block');
-//     newBlock.id = currentid + "cl";
-//     newBlock.className = 'block';
-//     currentid += 1;
-//     this.parent.appendChild(newBlock);
-//     this.makeDraggable(newBlock);
-//     this.element = newBlock;
-//     const tenIhitbox = document.createElement('hitbox');
-//     newBlock.appendChild(tenIhitbox);
-//     this.hitbox = tenIhitbox;
-//     this.hitbox.id = (currentid -  1) + 'h';
-//     tenIhitbox.className = 'hitbox';
-//     const vvod = document.createElement('input')
-//     vvod.addEventListener("input", function() {
-//       this.data = this.value
-//     })
-//     this.element.appendChild(vvod);
-//     vvod.style.height = 100 + 'px'
-//     vvod.style.width = 50 + 'px'
-
-
-
-//   };
-
-//   getAbsoluteCoordinates(parentId, childId) {
-//     // Получаем элементы по их ID
-//     const parentElement = document.body;
-//     const childElement = document.getElementById(childId);
-
-//     // Если одного из элементов нет, выводим предупреждение
-//     if (!parentElement || !childElement) {
-//         console.warn('Элемент не найден. Проверьте правильность ID.');
-//         return;
-//     }
-
-//     // Получаем координаты родительского блока
-//     const parentRect = parentElement.getBoundingClientRect();
-
-//     // Получаем координаты дочернего блока
-//     const childRect = childElement.getBoundingClientRect();
-
-//     // Вычисляем абсолютные координаты дочернего элемента относительно родителя
-//     const absoluteTop = childRect.top;
-//     const absoluteLeft = childRect.left;
-
-//     // Возвращаем результат
-//     return {
-//         y: this.element.offsetTop,
-//         x: this.element.offsetLeft
-//     };
-//   };
-
-//   contact(e, msize)
-//   {
-//     var cords = this.getAbsoluteCoordinates(document.body.id,this.element.id);
-//     cords.x += this.size * 0.75
-//     cords.xp = cords.x + msize;
-//     cords.yp = cords.y + this.size;
-//     const mouseX = pos.clientX;
-//     const mouseY = pos.clientY;
-//     console.log(cords, mouseX, mouseY);
-//     console.log(cords, mouseX, mouseY)
-//     if ((cords.x < mouseX && mouseX < cords.xp &&  cords.y < mouseY && mouseY < cords.yp))
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//       return false;
-//     }
-
-//     };
-//   }
-//     createInput()
-//     {
-//       this.inputs = []
-
-
-
-//     }
-
-
-
-// // function makeDraggable(element) {
-// //     let isDragging = false;
-// //     let offsetX, offsetY;
-
-// //     element.addEventListener('mousedown', (e) => {
-// //       isDragging = true;
-// //       offsetX = e.clientX - element.offsetLeft;
-// //       offsetY = e.clientY - element.offsetTop;
-// //     });
-
-// //     document.addEventListener('mouseup', () => {
-// //       isDragging = false;
-// //     });
-
-// //     document.addEventListener('mousemove', (e) => {
-// //       if (!isDragging) return;
-// //       element.style.left = (e.clientX - offsetX) + 'px';
-// //       element.style.top = (e.clientY - offsetY) + 'px';
-// //     });
-// //   }
-
-// // function createBlock(tip, params, parent)
-// // {
-// //     const newBlock = document.createElement('block');
-// //     newBlock.id = currentid;
-// //     currentid += 1;
-// //     newBlock.dataset.type = tip;
-// //     newBlock.dataset.params = params;
-// //     const tenIhitbox = document.createElement('hitbox')
-// //     parent.appendChild(newBlock);
-// //     makeDraggable(newBlock);
-
-// // }
 function checkSquaresIntersection(square1, square2) {
   // Функция для проверки, находится ли точка внутри прямоугольника (не квадрата)
   function isPointInsideRect(point, rect) {
@@ -398,6 +174,7 @@ class Block {
   moveToAbsolute(x, y) {
     this.element.style.left = x + 'px';
     this.element.style.top = y + 'px';
+    console.log(this.getAbsoluteCoordinates())
   }
 
   /**
@@ -466,7 +243,7 @@ class Block {
       [thiscord.x, thiscord.y + 100]
     ]
     const square2 = [
-      [othercord.x, othercord.y],
+      [othercord.x - 1, othercord.y],
       [othercord.x + widhtHitbox, othercord.y],
       [othercord.x + widhtHitbox, othercord.y + 100],
       [othercord.x, othercord.y + 100]
@@ -511,6 +288,7 @@ const parentContainer = document.getElementById('container');
 // Создайте тестовые блоки
 testProgram[0].forEach(params =>  {
   const newBlock = new Block(params.type, [{"pain": 1}, {"pain": 2}, {test: 3}], parentContainer, blocks);
+  newBlock.moveToAbsolute(count * 100, count * 250)
   // blocks.push(newBlock);
 });
 
